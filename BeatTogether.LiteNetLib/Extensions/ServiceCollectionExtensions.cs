0a26c58bb5c4358a790d702325266adf4fbd666c
MT <littlehappytroll@gmail.com>
2021-12-10 09:26:19 -0500

    Added DI extension that adds litenet messaging

---
+++ BeatTogether.LiteNetLib/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using BeatTogether.LiteNetLib.Abstractions;
+using BeatTogether.LiteNetLib.Handlers;
+using BeatTogether.LiteNetLib.Headers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BeatTogether.LiteNetLib.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddLiteNetMessaging(this IServiceCollection services) =>
+            services
+                .AddSingleton<LiteNetPacketRegistry>()
+                .AddTransient<IPacketHandler<AckHeader>, AckPacketHandler>()
+                .AddTransient<IPacketHandler<BroadcastHeader>, BroadcastPacketHandler>()
+                .AddTransient<IPacketHandler<ChanneledHeader>, ChanneledPacketHandler>()
+                .AddTransient<IPacketHandler<ConnectRequestHeader>, ConnectRequestHandler>()
+                .AddTransient<IPacketHandler<DisconnectHeader>, DisconnectPacketHandler>()
+                .AddTransient<IPacketHandler<MtuCheckHeader>, MtuCheckPacketHandler>()
+                .AddTransient<IPacketHandler<PingHeader>, PingHandler>()
+                .AddTransient<IPacketHandler<PongHeader>, PongHandler>()
+                .AddTransient<IPacketHandler<UnconnectedHeader>, UnconnectedPacketHandler>()
+                .AddTransient<IPacketHandler<UnreliableHeader>, UnreliablePacketHandler>();
+    }
+}
