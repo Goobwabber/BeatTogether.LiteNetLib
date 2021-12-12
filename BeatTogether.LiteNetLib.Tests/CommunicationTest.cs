5fbd73caae25488a79a5f674f6205f7bed460cdb
MT <littlehappytroll@gmail.com>
2021-12-12 01:55:49 -0500

    Add new tests

--- BeatTogether.LiteNetLib.Tests/CommunicationTest.cs
+++ BeatTogether.LiteNetLib.Tests/CommunicationTest.cs
@@ -87,18 +87,16 @@ namespace BeatTogether.LiteNetLib.Tests
         }
 
         [Test, Timeout(TestTimeout)]
-        public void ServerDeliveryTest()
+        public void DeliveryFromServerTest()
         {
             bool msgDelivered = false;
             bool msgReceived = false;
-            const int testSize = 250; //250 * 1024;
+            const int testSize = 250;
             _serverListener.ConnectedEvent += endPoint =>
             {
                 msgDelivered = true;
                 byte[] arr = new byte[testSize];
                 arr[0] = 196;
-                //arr[7000] = 32;
-                //arr[12499] = 200;
                 arr[testSize - 1] = 254;
                 _server.Send(endPoint, new ReadOnlySpan<byte>(arr));
             };
@@ -107,8 +105,6 @@ namespace BeatTogether.LiteNetLib.Tests
                 // 4 is the size of the channeled message header
                 Assert.AreEqual(testSize, data.RawDataSize - 4);
                 Assert.AreEqual(196, data.RawData[0 + 4]);
-                //Assert.AreEqual(32, data.RawData[7000 + 4]);
-                //Assert.AreEqual(200, data.RawData[12499 + 4]);
                 Assert.AreEqual(254, data.RawData[testSize + 4 - 1]);
                 Assert.AreEqual(DeliveryMethod.ReliableOrdered, method);
                 msgReceived = true;
@@ -133,7 +129,92 @@ namespace BeatTogether.LiteNetLib.Tests
         }
 
         [Test, Timeout(TestTimeout)]
-        public void ClientDeliveryTest()
+        public void DeliveryFromClientTest()
+        {
+            bool msgDelivered = false;
+            bool msgReceived = false;
+            const int testSize = 250;
+            _clientNetListener.DeliveryEvent += (peer, obj) =>
+            {
+                Assert.AreEqual(5, (int)obj);
+                msgDelivered = true;
+            };
+            _clientNetListener.PeerConnectedEvent += peer =>
+            {
+                int testData = 5;
+                byte[] arr = new byte[testSize];
+                arr[0] = 196;
+                arr[testSize - 1] = 254;
+                peer.SendWithDeliveryEvent(arr, 0, DeliveryMethod.ReliableUnordered, testData);
+            };
+            _serverListener.ReceiveConnectedEvent += (endPoint, data, method) =>
+            {
+                var reader = new SpanBufferReader(data);
+                Assert.AreEqual(testSize, reader.RemainingSize);
+                Assert.AreEqual(196, reader.RemainingData[0]);
+                Assert.AreEqual(254, reader.RemainingData[testSize - 1]);
+                msgReceived = true;
+            };
+
+            _clientNetManager.Connect("127.0.0.1", TestServer.Port, "");
+
+            while (_clientNetManager.ConnectedPeersCount != 1 || !msgDelivered || !msgReceived)
+            {
+                Thread.Sleep(15);
+                _clientNetManager.PollEvents();
+            }
+
+            Assert.AreEqual(1, _clientNetManager.ConnectedPeersCount);
+        }
+
+        [Test, Timeout(TestTimeout)]
+        public void FragmentFromServerTest()
+        {
+            bool msgDelivered = false;
+            bool msgReceived = false;
+            const int testSize = 250 * 1024;
+            _serverListener.ConnectedEvent += endPoint =>
+            {
+                msgDelivered = true;
+                byte[] arr = new byte[testSize];
+                arr[0] = 196;
+                arr[7000] = 32;
+                arr[12499] = 200;
+                arr[testSize - 1] = 254;
+                _server.Send(endPoint, new ReadOnlySpan<byte>(arr));
+            };
+            _clientNetListener.NetworkReceiveEvent += (endPoint, data, method) =>
+            {
+                // 4 is the size of the channeled message header
+                Assert.AreEqual(testSize, data.RawDataSize - 4);
+                Assert.AreEqual(196, data.RawData[0 + 4]);
+                Assert.AreEqual(32, data.RawData[7000 + 4]);
+                Assert.AreEqual(200, data.RawData[12499 + 4]);
+                Assert.AreEqual(254, data.RawData[testSize + 4 - 1]);
+                Assert.AreEqual(DeliveryMethod.ReliableOrdered, method);
+                msgReceived = true;
+            };
+
+            _clientNetManager.Connect("127.0.0.1", TestServer.Port, "");
+
+            while (_clientNetManager.ConnectedPeersCount != 1)
+            {
+                Thread.Sleep(15);
+                _clientNetManager.PollEvents();
+            }
+
+            while (!msgDelivered || !msgReceived)
+            {
+                Thread.Sleep(15);
+                _clientNetManager.PollEvents();
+                Assert.AreEqual(1, _clientNetManager.ConnectedPeersCount);
+            }
+
+            Assert.AreEqual(1, _clientNetManager.ConnectedPeersCount);
+        }
+
+        [Test, Timeout(TestTimeout)]
+        public void FragmentFromClientTest()
         {
             bool msgDelivered = false;
             bool msgReceived = false;
