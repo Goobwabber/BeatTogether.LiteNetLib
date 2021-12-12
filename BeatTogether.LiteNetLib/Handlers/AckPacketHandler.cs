f47253e7851d03630700a0dc9027daad6440a942
MT <littlehappytroll@gmail.com>
2021-12-12 00:41:36 -0500

    add acknowledgement handler

--- BeatTogether.LiteNetLib/Handlers/AckPacketHandler.cs
+++ BeatTogether.LiteNetLib/Handlers/AckPacketHandler.cs
@@ -9,9 +9,19 @@ namespace BeatTogether.LiteNetLib.Handlers
 {
     public class AckPacketHandler : BasePacketHandler<AckHeader>
     {
+        private readonly LiteNetReliableDispatcher _reliableDispatcher;
+
+        public AckPacketHandler(
+            LiteNetReliableDispatcher reliableDispatcher)
+        {
+            _reliableDispatcher = reliableDispatcher;
+        }
+
         public override Task Handle(EndPoint endPoint, AckHeader packet, ref SpanBufferReader reader)
         {
-            throw new NotImplementedException(); // TODO
+            foreach (int acknowledgement in packet.Acknowledgements)
+                _reliableDispatcher.Acknowledge(endPoint, packet.ChannelId, acknowledgement);
+            return Task.CompletedTask;
         }
     }
 }
