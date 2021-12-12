fa25168cb68def4a1f8ef8c6cfa41d3533dd02f9
MT <littlehappytroll@gmail.com>
2021-12-12 00:42:44 -0500

    add fragment stuff to channeled header

--- BeatTogether.LiteNetLib/Headers/ChanneledHeader.cs
+++ BeatTogether.LiteNetLib/Headers/ChanneledHeader.cs
@@ -9,12 +9,22 @@ namespace BeatTogether.LiteNetLib.Headers
         public override PacketProperty Property { get; set; } = PacketProperty.Channeled;
         public ushort Sequence { get; set; }
         public byte ChannelId { get; set; }
+        public ushort FragmentId { get; set; }
+        public ushort FragmentPart { get; set; }
+        public ushort FragmentsTotal { get; set; }
 
         public override void ReadFrom(ref SpanBufferReader bufferReader)
         {
             base.ReadFrom(ref bufferReader);
             Sequence = bufferReader.ReadUInt16();
             ChannelId = bufferReader.ReadUInt8();
+
+            if (IsFragmented)
+            {
+                FragmentId = bufferReader.ReadUInt16();
+                FragmentPart = bufferReader.ReadUInt16();
+                FragmentsTotal = bufferReader.ReadUInt16();
+            }
         }
 
         public override void WriteTo(ref SpanBufferWriter bufferWriter)
@@ -22,6 +32,13 @@ namespace BeatTogether.LiteNetLib.Headers
             base.WriteTo(ref bufferWriter);
             bufferWriter.WriteUInt16(Sequence);
             bufferWriter.WriteUInt8(ChannelId);
+
+            if (IsFragmented)
+            {
+                bufferWriter.WriteUInt16(FragmentId);
+                bufferWriter.WriteUInt16(FragmentPart);
+                bufferWriter.WriteUInt16(FragmentsTotal);
+            }
         }
     }
 }
